71a983c80fb3457ede24ee326ca9200a666af2b6
liuyingxincindy <118701430+liuyingxincindy@users.noreply.github.com>
2023-01-09 09:44:04 +0800

    push these files

---
+++ lesson1.js
@@ -0,0 +1,54 @@
+/*
+
+1.Comments
+2.Variables
+3. Data Types
+4. Concatenation
+5.
+
+
+
+
+
+
+
+/*
+Multi
+Line
+Comments
+*/
+
+//Variables
+
+//permanent
+const age = 7
+const year= 2023
+
+// changeable
+let favNum = 18
+
+favNum = 12
+favNum = 7
+
+//Data Types
+
+//Number Data Type
+const birthYear = 2007
+
+// String Data Type
+const student1 = "Cindy"
+const student2 = 'Ariel'
+let  student3 = `An`
+
+// Concatenation
+// joining things together
+
+//using +
+document.write ("Hi" + student1)
+document.write ( student1 + " is born in "+ birthYear)
+
+// using ``
+document.write (`${student3} is born in ${birthYear} and we are friends with ${student2}`)
+
+//Prompts
+ const event=prompt('What will you do in the Chinese New Year ? ')
