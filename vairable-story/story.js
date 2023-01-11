c44245b76f7d22d0862fb6253052950a8934492e
liuyingxincindy <118701430+liuyingxincindy@users.noreply.github.com>
2023-01-11 23:59:59 +0800

    ya!

---
+++ vairable-story/story.js
@@ -0,0 +1,54 @@
+// Prompts
+const intro = prompt('Do you want to listen a story ?')
+//Data Types
+
+// String data types
+const pig1 = "first little pig"
+const pig2 = 'second little pig'
+const pig3 = "third little pig"
+const pig4 = "Mother Pig"
+const wolf = " wolf"
+
+// Contatenation
+document.write  (`Once upon a time, there were three little pigs in the forest.There are ${pig1}, ${pig2}, and ${pig3}.` ) 
+document.write  (`Their ${pig4} talked to them that there were not enough food for them to eat, so she sent them out to seek their fortunes `)
+document.write  (`<p>Brother Pig 1 met a man carrying a bundle of straw.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;"Excuse me, would you please sell some of your straw so I can make a house?", said the ${pig1} politely. </p>`)
+document.write (`<p>The man readily agreed and the ${pig1} went off to find a good place to build his house. </p>`)
+document.write (`<p>The other little pigs carried on along the road and, soon, they met a man carrying a bundle of sticks</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Excuse me,'said the little pig politely. 'Would you please sell me some sticks so I can build a house?'</p>`)
+document.write (`<p>The man readily agreed and the little pig said goodbye to his brother.</p>`)
+document.write (`<p>The third little pig didn't think much of their ideas:</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'I'm going to build myself a much bigger, better, stronger house,' he thought, and he carried off down the road until he met a man with a cart load of bricks.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Excuse me,' said the ${pig3} , as politely as his mother had taught him. 'Please can you sell me some bricks so I can build a house?</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Of course,' said the man. 'Where would you like me to unload them?</p>`)
+document.write (`<p>The third little pig looked around and saw a nice patch of ground under a tree.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Over there,' he pointed.`)
+document.write (`<p>They all set to work and by nighttime the house of straw and the house of sticks were built but the house of bricks was only just beginning to rise above the ground. The first and second little pigs laughed, they thought their brother was really silly having to work so hard when they had finished.</p>`)
+document.write (`<p>However, a few days later the brick house was completed and looked very smartwith shiny windows, a neat little chimney and a shiny knocker on the door.</p>`)
+document.write (`<p>One starlit night, soon after they had settled in, a wolf came out looking for food. By the light of the moon he espied the first little pig's house of straw and he sidled up to the door and called: </p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Little pig, little pig, let me come in.'<p/>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'No, no, by the hair of my chinny chin chin!' replied the little pig.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Then I'll huff and I'll puff and l'11 blow your house in!' said the wolf who was a very big, bad, and a greedy sort of wolf.</p>`)
+document.write (`<p>And he huffed, and he puffed and blew the house in. But the little pig ran away as fast as his trotters could carry him and went to the second little pig's house to hide.</p>`)
+document.write (`<p>The next night the wolf was even hungrier and he saw the house of sticks. He crept up to the door and called:</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Little pig, little pig, let me come in.'</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Oh no, not by the hair on my chinny chin chin!' said the second little pig, as the first little pig hid trembling under the stairs.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Then I'll huff and I'll puff and I'll blow your house in!' said the wolf.</p>`)
+document.write (`<p> And he huffed, and he puffed and he blew the house in. But the little pigs ran away as fast as their trotters could carry them and went to the third little pig's house to hide </p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'What did I tell you?' said the third little pig. 'It's important to build houses properly.' But he welcomed them in and they all settled down for the rest of the night.</p>`)
+document.write (`<p>The following night the wolf was even hungrier and feeling bigger and badder than ever.</p>`)
+document.write (`<p>Prowling around, he came to the third little pig's house. He crept up to the door and called: </p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Little pig, little pig, let me come in.'</p`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Oh no, not by the hair on my chinny chin chin!' said the third little pig, while the first and the second little pigs hid trembling under the stairs.</p>`)
+document.write (`<p>'Then I'll huff and I'll puff and I'll blow your house in!' said the wolf.`)
+document.write (`<p>And he huffed, and he puffed and he blew but nothing happened. So he huffed and he puffed and he blew again, even harder, but still nothing happened. The brick house stood firm.</p>`)
+document.write (`<p>The ${wolf} was very angry and getting even bigger and even badder by the minute.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'I'm going to eat you all,' he growled, 'just you wait and see.'</p>`)
+document.write (`<p>He prowled round the house trying to find a way in. The little pigs trembled when they saw his big eyes peering through the window. Then they heard a scrambling sound.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Quick, quick!' said the ${pig3}. 'He's climbing the tree. I think he's going to come down the chimney.'</p>`)
+document.write (`<p>The three little pigs got the biggest pan they had, and filled it full of water and put it on the fire to boil. All the time they could hear the sound of the ${wolf} climbing the tree and then walking along the roof.</p>`)
+document.write (`<p>The little pigs held their breath. The ${wolf} was coming down the chimney. Nearer and nearer he came until, with a tremendous splash, he landed in the pan of water.</p>`)
+document.write (`<p>&nbsp;&nbsp;&nbsp;&nbsp;'Yoweeeee!' he screamed, and shot back up the chimney thinking his tail was on fire.</p>`)
+document.write (`<p>The last the three little pigs saw of the big bad ${wolf} was him flying over the treetops clutching a very sore tail.</p>`)
+document.write (`<p>So, the three little pigs lived happily together ever after in their very smart houses of bricks.</p>`)
