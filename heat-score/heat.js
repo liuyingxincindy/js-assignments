14c59b382712628af1f038915eb2618c1839e1cf
liuyingxincindy <118701430+liuyingxincindy@users.noreply.github.com>
2023-01-31 20:46:38 +0800

    update

--- heat-score/heat.js
+++ heat-score/heat.js
@@ -1,5 +1,5 @@
 const name = prompt("What's your name ?")
-const gender = prompt("What is your gender, boys or girls?")
+const gender = prompt("What is your gender, boy or girl ?")
 
 
 if (gender ==="boy"){
@@ -20,16 +20,16 @@ if (gender ==="boy"){
     }
 
     let total = hair + personality + rich 
-    document.write(`<p class = "p1"> Hi ${name}, your total score is ${total} </p>`)
+    document.write(`<p class = "p1"> Hi ${name}, your total score is ${total} of 30 </p>`)
 
     if (total > 21){
-        document.write(`<p class="p2"> Good job !</p>`)
+        document.write(`<p class = "p2"> Good job !</p>`)
     
     } else if (total <= 21 && total >= 11){
-        document.write(`<p class="p2"> Not too bad, but it can be better </p>`)
+        document.write(`<p class = "p2"> Not too bad, but it can be better </p>`)
     
     } else if ( total < 11){
-        document.write(`<p class="p2"> Not good </p>`)
+        document.write(`<p class = "p2"> Not good </p>`)
     }
 
 
@@ -50,15 +50,15 @@ if (gender ==="boy"){
 
  let total2 = hair2 + appearance + mommy 
  
- document.write (`<p> class="p1"> Hi ${name}, your total score is ${total2} </p>`)
+ document.write (`<p class = "p1"> Hi ${name}, your total score is ${total2} of 30 </p>`)
 
  if (total2 > 21){
-    document.write(`<p class="p2"> Good job !</p>`)
+    document.write(`<p class = "p2"> Good job !</p>`)
 
 } else if (total2 <= 21 && total2 >= 11){
-    document.write(`<p class="p2"> Not too bad, but it can be better </p>`)
+    document.write(`<p class = "p2"> Not too bad, but it can be better </p>`)
 
 } else if ( total2 < 11){
-    document.write(`<p class="p2"> Not good </p>`)
+    document.write(`<p class = "p2" > Not good </p>`)
 }
 }
