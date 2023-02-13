3ff766ad205f1d7af3df69d5067ffe30e89022af
liuyingxincindy <118701430+liuyingxincindy@users.noreply.github.com>
2023-02-13 09:51:39 +0800

    go

---
+++ Loops of Doom/loops.js
@@ -0,0 +1,88 @@
+document.write (`<p> Challenge 1 </p>`)
+
+
+
+for(i = 0; i <5; i++){
+    document.write(`I love web dev class! , `)
+}
+
+document.write (`<p> Challenge 2 </p>`)
+
+
+for(i = 0; i < 5; i++) {
+    document.write(i)
+}
+
+document.write(`<p> Challenge 3 </p>`)
+
+
+for(i = 15; i <33; i = i + 3){
+    document.write(i,)
+}
+
+document.write(`<p> Challenge 4 </p>`)
+
+
+
+
+for(i = 0; i < 4; i = i + 1 ) {
+    
+    for(j = 0; j < 4; j = j + 1){
+      document.write(`⬜`)
+    }
+    document.write('<br>')
+}
+
+document.write(`<p> Challenge 5`)
+
+document.write('<br>')
+
+
+for(i = 0; i < 1; i = i + 1){
+
+    for(j = 0; j < 1; j++){
+    document.write(`🦴`)
+    }
+    document.write(`<br>`)
+
+    for(j = 0; j < 2; j++){
+        document.write(`🦴`)
+        }
+        document.write(`<br>`)
+
+        for(j = 0; j < 3; j++){
+            document.write(`🦴`)
+            }
+            document.write(`<br>`)
+
+            for(j = 0; j < 4; j++){
+                document.write(`🦴`)
+                }
+                document.write(`<br>`)
+
+                for(j = 0; j < 5; j++){
+                    document.write(`🦴`)
+                    }
+                    document.write(`<br>`)
+
+                    for(j = 0; j < 6; j++){
+                        document.write(`🦴`)
+                        }
+                        document.write(`<br>`)
+
+                        for(j = 0; j < 7; j++){
+                            document.write(`🦴`)
+                            }
+                            document.write(`<br>`)
+}
+
+
+document.write(`<p> Challenge 6`)
+
+document.write('<br>')
+
+for(a = 0; a <7; a++){
+
+}
+
+
